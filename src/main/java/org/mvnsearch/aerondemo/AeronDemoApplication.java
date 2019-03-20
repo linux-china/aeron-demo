2f9a18b8c0f603775e88579aae48d149b96bc921
linux_china <libing.chen@gmail.com>
2019-03-20 13:34:18 -0700

    Aeron Demo app

---
+++ src/main/java/org/mvnsearch/aerondemo/AeronDemoApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.aerondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeronDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeronDemoApplication.class, args);
+	}
+}
